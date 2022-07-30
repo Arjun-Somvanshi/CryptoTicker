434b9aef5984ee85541b8225db851f8f85df2de4
Arjun-Somvanshi <arjunsomvanshi@gmai.com>
2022-07-30 12:26:03 +0530

    directory structuring

--- app/sendemails.py
+++ app/sendemails.py
@@ -12,7 +12,7 @@ def send_email(user_email, alert_id, alert_target):
 
         smtp.login(EMAIL, PASSWORD)
         subject = f"Your Alert has hit the target!"
-        body = f"Alert: {alert_id} has been triggered! React soon. Bitcoin is at: {alert_target}"
+        body = f"Alert: {alert_id} has been triggered! React soon. Bitcoin is at: ${alert_target}"
         msg = f'Subject: {subject}\n\n{body}'
         smtp.sendmail(EMAIL, user_email, msg)
         print("Sent Mail")
